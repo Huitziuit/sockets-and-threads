0f6fc97e5ac4722c04090276da0fee2c286743a1
Huitziuit <e.huitzilopochtli@gmail.com>
2021-11-30 01:17:39 -0600

    calculo en threads

--- express2Server.py
+++ express2Server.py
@@ -2,37 +2,57 @@ import random
 import math
 from sys import argv
 import sys
+from threading import Thread, Lock
+import time
+
+
+clientes=[]
+
 
 def main():
     argv=sys.argv
 
-    numAutos=200000
-    anchoDelMapa=1000000000
-
-    mejorUber=0
+    numAutos=10000000       #10000000            tarda 26seg
+    anchoDelMapa=10000000000    #10,000,000,000
 
     #Coloca en lugares aleatorios los autos
     PosAutosX=[]
     PosAutosY=[]
 
+    print("Colocando autos...")
     for i in range(numAutos):
         
         PosAutosX.append(random.randrange(0, anchoDelMapa, 1))
         PosAutosY.append(random.randrange(0, anchoDelMapa, 1))
+    print("Autos colocados")
+    print("\n\n========== Esperando clientes ==========\n")
 
-    #Encuentra la distancia mas corta el clienta a los autos
+    for i in range(10):
+        t = Thread(target=calcula, args=(PosAutosX,PosAutosY,numAutos,int(argv[1]),int(argv[2])))
+        t.start()
+    
+    time.sleep(60)
+    #Encuentra la distancia mas corta del cliente a los autos
+    #calcula(PosAutosX,PosAutosY,numAutos,int(argv[1]),int(argv[2]))
+
+
+
+def calcula(Xs,Ys,No_A,XUsuario,YUsuario): 
+    print("Calculando distancias a cada Uber...")
     menor=math.inf
-    for i in range(numAutos):
-        aux=distancia(int(argv[1]), int(argv[2]),PosAutosX[i],PosAutosY[i])
+    mejorUber=0
+    for i in range(No_A):
+        aux=distancia(XUsuario, YUsuario,Xs[i],Ys[i])
         if(aux<menor):
             menor=aux
             mejorUber=i
 
-    print("menor distancia ",menor, "Uber No. ",mejorUber+1 )  
+    print("mejor distancia ",menor, "Uber No. ",mejorUber+1 )  
+    
 
 def distancia(x1, y1, x2, y2):
     distancia = math.sqrt(pow((x2-x1),2)+pow((y2-y1),2))
-    print("distancia ", distancia)
+    #print("distancia ", distancia)
     return distancia
 
 if __name__ == "__main__":
